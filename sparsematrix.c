9b2f7960f2aeb836869a8776f9979b6f70eb43be
Varsha <varsha.aux@redgreen.io>
2021-05-25 22:44:08 +0530

    feat: show 3-tuple matrix for 2d array

--- sparsematrix.c
+++ sparsematrix.c
@@ -6,10 +6,18 @@ void main() {
     printf("Enter order of the matrix (should be less than 5x5): \n");
     scanf("%d%d", &rows, &columns);
     printf("Enter the elements: \n");
-
     for(i = 0; i < rows; i++) {
-        for(j = 0; j < rows; j++) {
-            scanf("%d", a[i][j]);
+        for(j = 0; j < columns; j++) {
+            scanf("%d", &a[i][j]);
         }
     }
+    printf("The 3-tuple representation is: \n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < columns; j++) {
+            if(a[i][j] != 0) {
+                printf("%d %d %d\n", (i + 1), (j + 1), a[i][j]);
+            }
+        }
+    }
+   
 }
