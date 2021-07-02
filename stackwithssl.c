0591c9d9af3eea79c03fd6341e630f98d1d510ef
Varsha Singh <varsha.singh0497@gmail.com>
2021-07-03 02:40:39 +0530

    feat: add push to stack function

--- stackwithssl.c
+++ stackwithssl.c
@@ -1,6 +1,9 @@
 /* implementation of a stack using a single linked list */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+void push();
 
 struct node {
     int data;
@@ -10,5 +13,16 @@ struct node {
 struct node * top = NULL;
 
 void main() {
-    
+    push(2);
+}
+
+void push(int value) {
+    struct node * newnode;
+    newnode = (struct node *)malloc(sizeof(struct node));
+
+    newnode -> data = value;
+    newnode -> next = top;
+    top = newnode;
+
+    printf("Value added!\n");
 }
