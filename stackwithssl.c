6dcfcb4d9079e500fb3b12e3f871c2a23d2331de
Varsha Singh <varsha.singh0497@gmail.com>
2021-07-03 02:33:02 +0530

    feat: create single linked list structure for stack

---
+++ stackwithssl.c
@@ -0,0 +1,14 @@
+/* implementation of a stack using a single linked list */
+
+#include <stdio.h>
+
+struct node {
+    int data;
+    struct node * next;
+};
+
+struct node * top = NULL;
+
+void main() {
+    
+}
