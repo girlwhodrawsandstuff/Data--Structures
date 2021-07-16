ff4f51aff8f00e8b6f80217d61bcc6947f068de6
Varsha Singh <varsha.singh0497@gmail.com>
2021-07-16 20:16:24 +0530

    feat:  delete element from stacked queue function

--- Queues/queuestack.c
+++ Queues/queuestack.c
@@ -5,6 +5,10 @@
 
 void addToQueue();
 void pushToFirstStack();
+void deleteFromQueue();
+int popFromFirstStack();
+int popFromSecondStack();
+void pushToSecondStack();
 
 int firstStack[CAPACITY], secondStack[CAPACITY];
 int top1 = -1, top2 = -1;
@@ -12,6 +16,7 @@ int count = 0;
 
 void main() {
     addToQueue(5);
+    deleteFromQueue();
 }
 
 void addToQueue(int value) {
@@ -26,5 +31,57 @@ void pushToFirstStack(int data) {
         top1++;
         firstStack[top1] = data;
         printf("'%d' was added to the queue.\n", data);
+    }
+}
+
+void deleteFromQueue() {
+    int i, a, b;
+    if(top1 == -1 && top2 == -1) {
+        printf("The queue is empty.\n");
+    } else {
+        for(i = 0; i < count; i++) {
+            a = popFromFirstStack();
+            pushToSecondStack(a);
+        }
+
+        b = popFromSecondStack();
+        printf("'%d' was deleted from the queue.\n", b);
+        count--;
+
+        for(i = 0; i < count; i++) {
+            a = popFromSecondStack();
+            pushToFirstStack(a);
+        }
+    }
+}
+
+int popFromFirstStack() {
+    if(top1 == -1 && top2 == -1) {
+        printf("The queue is empty.\n");
+    } else {
+        int a;
+        a = firstStack[top1];
+        top1--;
+        return a;
+    }
+}
+
+int popFromSecondStack() {
+    if(top1 == -1 && top2 == -1) {
+        printf("The queue is empty.\n");
+    } else {
+        int a;
+        a = secondStack[top2];
+        top2--;
+        return a;
+    }
+}
+
+void pushToSecondStack(int value) {
+    if(top2 == CAPACITY - 1) {
+        printf("The queue is full.\n");
+    } else{
+        top2++;
+        secondStack[top2] = value;
     }
 }
