54cc5474e5bc4e4e1a43d7d2ef50ddfbb90b5f3f
Varsha Singh <varsha.singh0497@gmail.com>
2021-07-20 17:25:32 +0530

    feat: add element to the rear of the deque

--- Queues/dequeuearray.c
+++ Queues/dequeuearray.c
@@ -8,11 +8,13 @@ int front = -1;
 int rear = -1;
 
 void enqueueFront();
+void enqueueRear();
 
 void main() {
     enqueueFront(5);
     enqueueFront(10);
-    enqueueFront(15);
+    enqueueRear(15);
+
 }
 
 void enqueueFront(int value) {
@@ -31,4 +33,23 @@ void enqueueFront(int value) {
         deque[front] = value;
         printf("'%d' was added to the front of the deque.\n", deque[front]);
     }
+}
+
+void enqueueRear(int value) {
+    if ((front == 0 && rear == CAPACITY - 1) || (front == rear + 1)) {
+        printf("The queue is full.\n");
+    } else if (front == -1 && rear == -1) {
+        front = rear = 0;
+        deque[rear] = value;
+        printf("'%d' was added to the rear of the deque.\n", deque[rear]);
+    } else if (rear == CAPACITY - 1) {
+        rear = 0;
+        deque[rear] = value;
+        printf("'%d' was added to the rear of the deque.\n", deque[rear]);
+    } else {
+        rear++;
+        deque[rear] = value;
+        printf("'%d' was added to the rear of the deque.\n", deque[rear]);
+    }
+    
 }
