2fe0574c584e22af91dccfa100cdbb4f6ae2766f
Varsha Singh <varsha.singh0497@gmail.com>
2021-07-21 20:35:00 +0530

    feat: create binary tree using recursive function

---
+++ Trees/binarytree.c
@@ -0,0 +1,39 @@
+/* implementation of a binary tree using doubly linked list*/
+/* recursive method */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node * left;
+    struct node * right;
+};
+
+struct node * create() {
+    int value;
+    struct node * newNode;
+    newNode = (struct node *)malloc(sizeof(struct node));
+
+    printf("Enter the data for the node. Enter -1 if you don't want to enter any value.\n");
+    scanf("%d", &value);
+
+    if (value == -1) {
+        return 0;
+    } else {
+        newNode -> data = value;
+
+        printf("Enter the data for the left node. Enter -1 if you don't want to enter any value.\n");
+        newNode -> left = create();
+
+        printf("Enter the data for the right node. Enter -1 if you don't want to enter any value.\n");
+        newNode -> right = create();
+
+        return newNode;
+    }
+}
+
+void main() {
+    struct node * root;
+    root = create();
+}
